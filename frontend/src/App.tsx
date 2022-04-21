8968a4b99906f2e4ac99c32b7822954541bfb4ab
stonesha-goarmstrong <36431006+stonesha@users.noreply.github.com>
2022-04-21 16:19:36 -0700

    changed status indicators and styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -57,9 +57,9 @@ function App() {
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.OPEN]: 'Online',
+    [ReadyState.CLOSING]: 'Disconnecting',
+    [ReadyState.CLOSED]: 'Offline',
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
@@ -68,19 +68,22 @@ function App() {
       <h1 className="text-4xl text-slate-400 text-center mb-2">cat-chat</h1>
       <div className="flex flex-row justify-around">
         <h2>Status: {connectionStatus}</h2>
-        <div className="flex flex-row m-0.5">
-          <input ref={lobbyRef} placeholder="Enter a lobby..." className="px-1 py-0.5 rounded-lg"/>
-          <button onClick={() => {
-            setConnect(true)
-            setLobby(lobbyRef.current!.value)
-          }}>Connect</button>
+        <div className="flex flex-row m-0.5 space-x-1">
+          <input ref={lobbyRef} placeholder="Enter a lobby..." className="px-1 py-0.5 rounded-lg bg-slate-200"/>
+          <button
+            className="rounded-lg px-1 py-0.5 bg-blue-400 text-white"
+            onClick={() => {
+              setConnect(true)
+              setLobby(lobbyRef.current!.value)
+            }}
+          >Connect</button>
         </div>
       </div>
       <hr />
       <div className="mx-20">
         {messages.map((message: string, index: number) => (
           <div key={index} className="mb-2">
-            <span className="text-slate-400">{message}</span>
+            <span className="text-slate-500">{message}</span>
           </div> 
         ))}
       </div>
